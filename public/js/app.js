9aa7ced69994d9c7eaeba90193010b2d0457aff8
Espen Hovlandsdal <espen@hovlandsdal.com>
2015-11-05 21:30:47 +0100

    Add actual JS logic for figuring out and rendering audio

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,75 @@
 'use strict';
 
-setTimeout(function() {
-    window.alert('Sup bro');
-}, 5000);
+var Modernizr = require('imports?window=>exports!./modernizr').Modernizr;
+var MorseNode = require('./morse-node');
+
+var useAudio = (window.location.search || '').indexOf('noaudio') === -1;
+var hasAudioApi = window.AudioContext || window.webkitAudioContext;
+
+if (useAudio) {
+    setupAudio(determineMethod());
+}
+
+function determineMethod() {
+    var urlMethod = (window.location.search || '').replace(/.*method=([a-z0-9]+).*/, '$1');
+    var ua = navigator.userAgent;
+    var iOS = /iPad/i.test(ua) || /iPhone/i.test(ua) || /iPod/i.test(ua);
+
+    if (urlMethod) {
+        return urlMethod;
+    } else if (hasAudioApi) {
+        return 'generated';
+    } else if (Modernizr.audio && Modernizr.audio.wav && !iOS) {
+        return 'html5audio';
+    } else if (iOS) {
+        return 'audioiframe';
+    } else if (window.morseMyIp.isOldIE) {
+        return 'bgsound';
+    }
+
+    return 'object';
+}
+
+function setupAudio(method) {
+    var audioControls = document.getElementById('audio-controls');
+
+    switch (method) {
+        case 'generated':
+            return playGeneratedAudio();
+        case 'html5audio':
+            return audioControls.appendChild(createAudioElement());
+        case 'audioiframe':
+            return audioControls.appendChild(createAudioIframe());
+        case 'object':
+            return audioControls.appendChild(createAudioObject());
+        default:
+            return;
+    }
+}
+
+function createAudioElement() {
+    var audio = document.createElement('audio');
+    audio.setAttribute('src', 'morse.wav');
+    audio.setAttribute('autoplay', 'autoplay');
+    return audio;
+}
+
+function createAudioIframe() {
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'morse.wav');
+    return iframe;
+}
+
+function createAudioObject() {
+    var object = document.createElement('object');
+    object.setAttribute('data', 'morse.wav');
+    return object;
+}
+
+function playGeneratedAudio() {
+    var ac = new (window.AudioContext || window.webkitAudioContext)();
+
+    var morse = new MorseNode(ac);
+    morse.connect(ac.destination);
+    morse.playString(ac.currentTime, window.morseMyIp.clientIP || '');
+}
