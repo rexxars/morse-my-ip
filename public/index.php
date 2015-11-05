c17be8aa92003133ea96b7f857a6e7525aec89a2
Espen Hovlandsdal <espen@hovlandsdal.com>
2015-11-05 21:31:54 +0100

    Fix markup and add morse generation PHP logic

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require __DIR__ . '/../library/bootstrap.php';
 $clientIp = getClientIp();
 $morsedIp = (new Morse\Text())->toMorse($clientIp);
 
+header('Cache-Control: no-cache');
 header('Content-Type: text/html; charset=utf-8');
 header('X-Your-IP: ' . $clientIp);
 header('X-Your-IP-Morse: ' . $morsedIp);
@@ -12,6 +13,7 @@ header('X-Your-IP-Morse: ' . $morsedIp);
 <html lang="en-us">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>What is my IP-address - Morse my IP.</title>
     <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
@@ -22,77 +24,44 @@ header('X-Your-IP-Morse: ' . $morsedIp);
 <div class="splash-container">
     <div class="splash">
         <h2>Your IP-address is:</h2>
-        <h1 class="splash-head"><?php echo $clientIp; ?></h1>
-        <p class="splash-subhead">
-            ... --- ...
-        </p>
+        <h1 class="splash-head">
+            <?php echo $clientIp; ?>
+            <div class="morse">
+            <?php
+            $blocks = explode(' ', $morsedIp);
+            echo '<span>' . implode('</span> <span>', $blocks) . '</span>';
+            ?>
+            </div>
+        </h1>
         <p>
             Turn on your speakers for the best experience ;-)
         </p>
 
-        <!--[if lte IE 7 ]><bgsound src="morse.php"><![endif]-->
+        <div id="audio-controls"></div>
+
+        <!--[if lte IE 7 ]>
+        <script>
+        window.morseMyIp = window.morseMyIp || {};
+        window.morseMyIp.isOldIE = true;
+        </script>
+        <bgsound src="morse.php">
+        <![endif]-->
     </div>
 </div>
 
+<script>
+window.morseMyIp = window.morseMyIp || {};
+window.morseMyIp.clientIP = <?php echo json_encode($clientIp); ?>
+</script>
+
 <div class="content-wrapper">
-    <div class="footer l-box is-center">
+    <div class="footer">
         Created by Espen Hovlandsdal (<a href="http://twitter.com/rexxars">@rexxars</a>)<br />
         <a href="http://espen.codes/">espen.codes</a>
     </div>
 </div>
 
 <script src="js/bundle.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
-<script src="js/modernizr-1.7.min.js"></script>
-<script>
-var enableSound = <?php echo isset($_GET['noaudio']) ? 'false' : 'true'; ?>;
-jQuery(document).ready(function(d){if(!enableSound){return}var b=navigator.userAgent;var c=/iPad/i.test(b)||/iPhone/i.test(b)||/iPod/i.test(b);if(Modernizr.audio&&Modernizr.audio.wav&&!c){d("#wrapper").append(d("<audio />",{src:"morse.php",autoplay:"autoplay"}))}else{if(c){d("#wrapper").append(d("<iframe />",{src:"morse.wav"}))}else{if(d.browser.msie&&parseInt(d.browser.version,10)<8){return}var a=function(e,f){d.ajax({url:e,cache:true,dataType:"script",success:f})};a("/js/swfobject.js",function(){a("/js/tinywav.js",function(){var e=document.createElement("div");e.setAttribute("id","TinyWavBlock");document.getElementById("wrapper").appendChild(e);var f={scale:"noscale",bgcolor:"#FFFFFF"};swfobject.embedSWF("/embed/wavplayer.swf?gui=none","TinyWavBlock","1","1","10.0.32.18","/embed/expressInstall.swf",{},f,f);window.TinyWav.init()})})}}});
-<?php
-/*
-jQuery(document).ready(function($) {
-    if (!enableSound) {
-        return;
-    }
-
-    var ua = navigator.userAgent;
-    var iOS = /iPad/i.test(ua) || /iPhone/i.test(ua) || /iPod/i.test(ua);
-
-    if (Modernizr.audio && Modernizr.audio.wav && !iOS) {
-        // HTML5 audio!
-        $('#wrapper').append($('<audio />', { src: 'morse.php', autoplay: 'autoplay' }));
-    } else if (iOS) {
-        // iOS requires click action, not in iframe though
-        $('#wrapper').append($('<iframe />', { src: 'morse.wav' }));
-    } else {
-        if ($.browser.msie && parseInt($.browser.version, 10) < 8) {
-            return; // bgsound :D
-        }
-
-        // Flash, then. Ugh.
-        var ls = function(url, callback) {
-            $.ajax({
-                url: url,
-                cache: true,
-                dataType: 'script',
-                success: callback
-            });
-        }
-
-        ls("/js/swfobject.js", function() {
-            ls("/js/tinywav.js", function() {
-                var player = document.createElement("div");
-                player.setAttribute("id", "TinyWavBlock");
-                document.getElementById('wrapper').appendChild(player);
-                var params = {'scale': 'noscale', 'bgcolor': '#FFFFFF'};
-                swfobject.embedSWF("/embed/wavplayer.swf?gui=none", "TinyWavBlock", "1", "1", "10.0.32.18", "/embed/expressInstall.swf", {}, params, params);
-                window.TinyWav.init();
-            });
-        });
-    }
-});
-*/
-?>
-</script>
 
 </body>
 </html>
---
+++ public/morse-wav.php
@@ -0,0 +1,55 @@
+<?php
+require __DIR__ . '/../library/bootstrap.php';
+
+$clientIp = getClientIp();
+$wavFile = (new Morse\Wav())->generate($clientIp);
+$contentLength = strlen($wavFile);
+
+header('Content-Type: audio/wav');
+header('X-Pad: avoid browser bug');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+header('Accept-Ranges: bytes');
+
+$seekStart = 0;
+$seekEnd = $contentLength;
+
+if (isset($_SERVER['HTTP_RANGE'])) {
+    list($sizeUnit, $rangeOrig) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+
+    $range = '';
+    if ($sizeUnit == 'bytes') {
+        // Multiple ranges could be specified at the same time,
+        // but for simplicity only serve the first range
+        $ranges = explode(',', $rangeOrig, 2);
+        $range = reset($ranges);
+    }
+
+    // Figure out download piece from range (if set)
+    if (preg_match('#^\-\d+$#', $range)) {
+        $seek = (int) preg_replace('#[^\d]#', '', $range);
+        $seekStart = $contentLength - $seek;
+        $seekEnd = $contentLength;
+    } else if (preg_match('#^\d+\-$#', $range)) {
+        $seekStart = (int) $range;
+        $seekEnd = $contentLength;
+    } else if (preg_match('#^\d+\-\d+$#', $range)) {
+        list($seekStart, $seekEnd) = explode('-', $range);
+    }
+}
+
+// Set start and end based on range (if set), else set defaults. Also, check for invalid ranges.
+$seekEnd = (empty($seekEnd)) ? ($contentLength - 1) : min(abs(intval($seekEnd)), ($contentLength - 1));
+$seekStart = (empty($seekStart) || $seekEnd < abs(intval($seekStart))) ? 0 : max(abs(intval($seekStart)), 0);
+
+// Only send partial content header if downloading a piece of the file (IE workaround)
+if ($seekStart > 0 || $seekEnd < ($contentLength - 1)) {
+    header('HTTP/1.1 206 Partial Content');
+}
+
+$chunk = substr($wavFile, $seekStart, $seekEnd - $seekStart);
+
+header('Accept-Ranges: bytes');
+header('Content-Range: bytes ' . $seekStart . '-' . $seekEnd . '/' . $contentLength);
+header('Content-Length: ' . strlen($chunk));
+
+echo $chunk;
